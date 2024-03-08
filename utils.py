ed378190fff928dc4758c7e0787ffb83a397e752
Gris86 <annochka1986@gmail.com>
2024-03-08 17:13:47 +0300

    Добавлены вспомогательные функции

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os.path
+
+import note
+
+def init():
+    if not os.path.exists("notes"):
+        os.mkdir("notes")
+    else:
+        if not os.path.isdir("notes"):
+            raise NotADirectoryError("Файл с именем notes уже существует, однако, это не папка.")
+
+def read_note(filename: str) -> note.Note:
+    with open(os.path.join(os.getcwd(), 'notes', filename), 'r') as f:
+        return note.Note.from_json(f.read())
+
+def write_note_to_file(note: note.Note, filename: str):
+    with open(os.path.join(os.getcwd(), 'notes', filename), 'w') as f:
+        f.write(note.to_json())
+
+def write_note(note: note.Note):
+    "Имя файла автоматически генерируется."
+    write_note_to_file(note, note.note_id.hex + ".json")
+
+def list_note_filenames() -> list[str]:
+    return os.listdir("notes")
+
+def get_all_notes() -> list[note.Note]:
+    lst = []
+    for x in list_note_filenames():
+        lst.append(read_note(x))
+    return lst
